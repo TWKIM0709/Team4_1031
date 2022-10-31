05c51c7638934a1e81887475031c9592ba677b3c
TaeWooKim <dhkdtpdn@gmail.com>
2022-10-31 16:16:53 +0900

    commit2

---
+++ src/main/java/kr/or/team4/controller/MemberServlet.java
@@ -0,0 +1,64 @@
+package kr.or.team4.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MemberServlet() {
+        super();
+       
+    }
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1. 한글 처리
+				request.setCharacterEncoding("UTF-8");
+				
+				//2. 데이터 받기 (입력데이터 , command)
+				String requestURI = request.getRequestURI();
+				String contextPath = request.getContextPath();
+				String urlcommand = requestURI.substring(contextPath.length());
+				
+				/*
+				 * 	주소가 http://192.168.0.22:8090/WebServlet/register.do 라면
+				 * requestURI 		: /WebServlet/register.do
+				 * contextPath 	: WebServlet
+				 * urlcommand	: /register.do
+				 * 
+				 * URL 마지막 주소를 추출하고 판단의 근거로 삼기
+				 */
+				
+				String viewpage = "";
+				//3. 요청 서비스 판단 (command 값을 비교해서)
+				//4. 데이터 저장 
+				if(urlcommand.equals("")) {
+					viewpage="";
+				} else if(urlcommand.equals("")) {
+					// 처리
+				
+					//뷰 설정하기
+				}
+				// ... else if 반복
+				//5. View 지정
+				RequestDispatcher dis = request.getRequestDispatcher(viewpage);
+				
+				dis.forward(request, response);
+				//forward
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
